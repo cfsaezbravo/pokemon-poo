19401c9cbe164238f61dcf8523333eedf66c1ccd
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-04 12:48:29 -0300

    Adding variables andfunctions

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -23,6 +23,15 @@ function Pokemon(nombre,color,vida,poderDeAtaque){
 var Pikachu = new Pokemon("Pikachu","amarillo",200,50);
 var Charmander = new Pokemon("Charmander", "rojo", 300,100);
 
-Pikachu.atacar(Charmander);
+var pokemon1 = document.getElementById("Pokemon1");
+var pokemon2 = document.getElementById("Pokemon2");
 
-console.log(Charmander.vida);
+var poke1 = pokemon1.options[pokemon1.selectedIndex].class;
+var poke2 = pokemon2.options[pokemon2.selectedIndex].class;
+
+function pelea(poke1, poke2){
+	alert("Hola");
+}
+
+Pikachu.atacar(Charmander);
+console.log("Nueva vida " + Charmander.vida);
